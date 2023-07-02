4ba651900389d48b5d1f15d3aaaac23233e28adf
Rodolfo Brizzi <rodbrizzi@gmail.com>
2023-07-02 18:54:06 -0300

    adicionado resultado.txt

--- criador_de_processos.py
+++ criador_de_processos.py
@@ -5,39 +5,59 @@ import time
 import socket
 
 class Processo(threading.Thread):
-    def __init__(self, id, host='localhost', port=12345):
+    def __init__(self, host='localhost', port=12345):
         threading.Thread.__init__(self)
-        self.id = id
+        self.pid = ''.join(random.choices(string.digits, k=6))
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((host, port))
 
     def run(self):
+        self.create_file()
         self.request()
         self.regiao_critica()
         self.release()
+
+    def create_file(self):
+        with open('resultado.txt', 'w') as arquivo:
+            arquivo.write('PID    | Hora Atual\n')
+            arquivo.close
+            time.sleep(3)
 
     def request(self):
         mensagem = self._criar_mensagem('1')
         self.client.send(mensagem.encode('utf-8'))
 
     def regiao_critica(self):
-        print('Processo', self.id, 'esperando GRANT')
+        print('Processo', self.pid, 'esperando GRANT')
         resposta = self.client.recv(1024).decode('utf-8')  # Espera pela resposta do coordenador
         if resposta == 'GRANT':
-            print('Processo', self.id, 'recebeu GRANT')
-            print('Processo', self.id, 'entrou na região crítica')
-            time.sleep(3)
-            print('Processo', self.id, 'saiu da região crítica')
+            print('Processo', self.pid, 'recebeu GRANT')
+            print('Processo', self.pid, 'entrou na região crítica')
+            with open('resultado.txt', 'a') as arquivo:
+                timestamp = time.time()
+                hora_atual = time.strftime("%H:%M:%S", time.localtime(timestamp))
+                milissegundos = int((timestamp - int(timestamp)) * 1000)
+                linha = f'{self.pid} | {hora_atual}.{milissegundos:03}\n'
+                arquivo.write(linha)
+                time.sleep(0)
+                arquivo.close
+            print('Processo', self.pid, 'saiu da região crítica')
 
     def release(self):
         mensagem = self._criar_mensagem('3')
         self.client.send(mensagem.encode('utf-8'))
 
     def _criar_mensagem(self, codigo):
-        return f'{codigo}|{self.id}|' + ''.join(['0' for _ in range(10 - len(codigo) - len(str(self.id)) - 2)])
+        return f'{codigo}|{self.pid}|' + ''.join(['0' for _ in range(10 - len(codigo) - len(str(self.pid)) - 2)])
 
 if __name__ == '__main__':
-    processo1 = Processo(10)
-    processo2 = Processo(20)
-    processo1.start()
-    processo2.start()
+    num_processos = 100  # Número de processos a serem criados
+
+    processos = []
+    for _ in range(num_processos):
+        processo = Processo()
+        processos.append(processo)
+        processo.start()
+
+    for processo in processos:
+        processo.join()
