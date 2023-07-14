8c0c576a6c5d55365a8f0dcb14f2c3d6789ab808
Rodolfo Brizzi <rodbrizzi@gmail.com>
2023-07-14 19:47:50 -0300

    projeto final

--- coordenador.py
+++ coordenador.py
@@ -5,30 +5,35 @@ import sys
 import threading
 from queue import Queue
 
+# Definição do endereço IP e Porta do servidor
+HOST = 'localhost'
+PORT = 12345
+
 class Coordenador:
-    def __init__(self, host='localhost', port=12345):
-        """Inicialização dos atributos do objeto."""
-        # Gerenciamento de thread
+    def __init__(self, host=HOST, port=PORT):
+        """Inicia os atributos do objeto."""
+        # Gerencia as threads
         self.is_running = True
         self.lock = threading.Lock()
 
-        # Configuração do servidor
+        # Configura o servidor
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((host, port))
         self.server.listen(5)
         signal.signal(signal.SIGINT, self.signal_handler)
 
-        # Gerenciamento de fila
+        # Gerencia a fila
         self.fila = Queue()
         self.grants_enviados = {}
 
-        # Inicia threads de processamento e interface
+        # Inicia as threads de processamento e interface
         self.process_thread = threading.Thread(target=self.process_requests)
         self.process_thread.start()
         self.interface_thread = threading.Thread(target=self.run_interface)
         self.interface_thread.start()
 
     def process_requests(self):
+        # Processa as requisições enquanto a aplicação estiver rodando
         while self.is_running:
             try:
                 client, addr = self.server.accept()
@@ -37,6 +42,7 @@ class Coordenador:
                 break
 
     def handle_client(self, client):
+        # Manipula a conexão com o cliente
         while True:
             mensagem = client.recv(1024).decode('utf-8')
             if not mensagem:
@@ -50,6 +56,7 @@ class Coordenador:
         client.close()
 
     def request(self, client, process_id):
+        # Processa uma requisição de um cliente
         with self.lock:
             if self.fila.empty():
                 self.update_grants_enviados(process_id)
@@ -57,6 +64,7 @@ class Coordenador:
             self.fila.put((client, process_id))
 
     def grant(self):
+        # Concede permissão para um cliente
         with self.lock:
             if not self.fila.empty():
                 next_process = self.fila.queue[0]
@@ -66,25 +74,30 @@ class Coordenador:
                 self.update_grants_enviados(next_process_pid)
 
     def release(self):
+        # Libera um recurso
         with self.lock:
             if not self.fila.empty():
                 self.fila.get()
 
     def update_grants_enviados(self, pid):
+        # Atualiza a contagem de GRANTs enviados
         if pid in self.grants_enviados:
             self.grants_enviados[pid] += 1
         else:
             self.grants_enviados[pid] = 1
 
     def _criar_mensagem(self, codigo, pid):
+        # Cria uma mensagem para o cliente
         return f'{codigo}|{pid}|' + ''.join(['0' for _ in range(10 - len(codigo) - len(str(pid)) - 2)])
 
     def signal_handler(self, sig, frame):
+        # Encerra a aplicação quando o sinal SIGINT é recebido
         self.is_running = False
         self.server.close()
         sys.exit(0)
 
     def run_interface(self):
+        # Roda a interface do usuário
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')  # Limpa o prompt de comando
             comando = input("Comandos:\n1 - Imprimir fila de pedidos atual\n2 - Imprimir quantas vezes cada processo foi atendido\n3 - Encerrar a execução\n\nDigite o número do comando: ")
@@ -103,20 +116,23 @@ class Coordenador:
                 input("Aperte qualquer tecla para continuar...")
 
     def print_current_queue(self):
+        # Imprime a fila atual
         with self.lock:
-            print("Fila de Pedidos:")
+            print("\nFila de Pedidos:\n")
             print("PID    | POSIÇÃO")
             for i, item in enumerate(self.fila.queue):
                 pid = item[1]
                 print(f"{pid} | {i+1}º")
 
     def print_grants_count(self):
+        # Imprime a quantidade de GRANTs por PID
         with self.lock:
-            print("PID    | QUANTIDADE DE GRANTS")
+            print("\nPID    | QUANTIDADE DE GRANTS")
             for pid, count in self.grants_enviados.items():
                 print(f"{pid} | {count}")
 
     def shutdown(self):
+        # Encerra a execução do coordenador
         self.is_running = False
         self.server.close()
         print("Programa finalizado.")
--- criador_de_processos.py
+++ criador_de_processos.py
@@ -4,86 +4,133 @@ import threading
 import time
 import socket
 from datetime import datetime
+import sys
+
+# Endereço IP e porta do servidor
+HOST = 'localhost'
+PORT = 12345
 
 class Processo(threading.Thread):
-    def __init__(self, host='localhost', port=12345, r=10, k=5):
+    def __init__(self, host=HOST, port=PORT, r=10, k=5):
         threading.Thread.__init__(self)
+        # Gera um identificador de processo único (PID)
         self.pid = ''.join(random.choices(string.digits, k=6))
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((host, port))
-        self.r = r  # número de vezes que o processo deve executar a região crítica
-        self.k = k  # tempo de espera em segundos após escrever no arquivo
-        self.tempo_na_regiao_critica = 0
-        self.start_time = None
-        self.end_time = None
-
+        self.r = r  # Define quantas vezes este processo deve acessar a região crítica
+        self.k = k  # Define quanto tempo (em segundos) este processo deve aguardar após acessar a região crítica
+        try:
+            # Cria uma conexão socket com o servidor
+            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client.connect((host, port))
+        except socket.error as e:
+            print("Erro de conexão: ", str(e))
+            sys.exit(1)
     def run(self):
-        self.create_file()
-        for _ in range(self.r):
-            self.request()
-            self.regiao_critica()
-            self.release()
-        self.client.close()
+        try:
+            # Inicia o arquivo de saída
+            self.create_file()
+            # O processo executa sua rotina r vezes
+            for _ in range(self.r):
+                self.request()  # O processo solicita acesso à região crítica
+                self.regiao_critica()  # O processo acessa a região crítica
+                self.release()  # O processo libera o acesso à região crítica
+        except socket.error as e:
+            print("Erro de conexão: ", str(e))
+            sys.exit(1)
+        finally:
+            # Fecha a conexão socket quando terminar
+            self.client.close()
 
     def create_file(self):
+        # Cria um arquivo chamado 'resultado.txt' para registrar as atividades do processo
         with open('resultado.txt', 'w') as arquivo:
             arquivo.write('PID    | Hora Atual\n')
-            arquivo.close()
             time.sleep(3)
 
     def request(self):
-        mensagem = self._criar_mensagem('1')
-        self.client.send(mensagem.encode('utf-8'))
+        # Envia uma solicitação ao servidor para acessar a região crítica
+        try:
+            mensagem = self._criar_mensagem('1')
+            self.client.send(mensagem.encode('utf-8'))
+        except socket.error as e:
+            print("Erro de conexão: ", str(e))
+            sys.exit(1)
 
     def regiao_critica(self):
-        resposta = self.client.recv(1024).decode('utf-8')  # Espera pela resposta do coordenador
-        if resposta.startswith('2'):  # GRANT
-            self.start_time = self.start_time or time.time()
-            start_time = time.time()
-            with open('resultado.txt', 'a') as arquivo:
-                timestamp = time.time()
-                hora_atual = time.strftime("%H:%M:%S", time.localtime(timestamp))
-                milissegundos = int((timestamp - int(timestamp)) * 1000)
-                linha = f'{self.pid} | {hora_atual}.{milissegundos:03}\n'
-                arquivo.write(linha)
-                time.sleep(self.k)
-            self.tempo_na_regiao_critica += time.time() - start_time
-            self.end_time = time.time()
+        # Aguarda a resposta do coordenador para acessar a região crítica
+        try:
+            resposta = self.client.recv(1024).decode('utf-8')  
+            if resposta.startswith('2'):  # Se a resposta começar com '2', então acesso é concedido (GRANT)
+                with open('resultado.txt', 'a') as arquivo:
+                    timestamp = time.time()
+                    hora_atual = time.strftime("%H:%M:%S", time.localtime(timestamp))
+                    milissegundos = int((timestamp - int(timestamp)) * 1000)
+                    linha = f'{self.pid} | {hora_atual}.{milissegundos:03}\n'
+                    arquivo.write(linha)
+                    time.sleep(self.k)  # Aguarda k segundos antes de liberar a região crítica
+        except socket.error as e:
+            print("Erro de conexão: ", str(e))
+            sys.exit(1)
 
     def release(self):
-        mensagem = self._criar_mensagem('3')
-        self.client.send(mensagem.encode('utf-8'))
+        # Envia uma mensagem ao servidor para liberar a região crítica
+        try:
+            mensagem = self._criar_mensagem('3')
+            self.client.send(mensagem.encode('utf-8'))
+        except socket.error as e:
+            print("Erro de conexão: ", str(e))
+            sys.exit(1)
 
     def _criar_mensagem(self, codigo):
-        return f'{codigo}|{self.pid}|' + ''.join(['0' for _ in range(10 - len(codigo) - len(str(self.pid)) - 2)])
+        # Cria uma mensagem para ser enviada ao servidor
+        return f'{codigo}|{self.pid}|'.ljust(10, '0')
+
+def get_integer_input(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Por favor, insira um número inteiro válido.")
+
+def main():
+    try:
+        n = get_integer_input("\nDigite a quantidade de processos a serem criados: ")  
+        r = get_integer_input("\nDigite quantas vezes cada processo deve acessar a região crítica: ")
+        k = get_integer_input("\nDigite o tempo em segundos para cada execução na região crítica: ")  
+        print("\nExecutando o programa...")
+
+        # Cria n processos
+        processos = [Processo(r=r, k=k) for _ in range(n)]
+
+        # Inicia todos os processos
+        for processo in processos:
+            processo.start()
+
+        # Aguarda todos os processos terminarem
+        for processo in processos:
+            processo.join()
+
+        # Escreve os parâmetros do experimento no arquivo de saída
+        with open('resultado.txt', 'a') as arquivo:
+            arquivo.write(f'\nQuantidade de processos(n): {n}\n')
+            arquivo.write(f'Quantidade de vezes que cada processo executou a região crítica(r): {r}\n')
+            arquivo.write(f'Tempo definido em segundos para cada execução na região crítica(k): {k}\n')
+
+        # Calcula o tempo total para a geração do arquivo 'resultado.txt'
+        with open('resultado.txt', 'r') as f:
+            lines = f.readlines()
+            first_line_time = lines[1].split('|')[1].strip()  
+            last_line_time = lines[-5].split('|')[1].strip() 
+
+            FMT = '%H:%M:%S.%f'
+            tdelta = datetime.strptime(last_line_time, FMT) - datetime.strptime(first_line_time, FMT)
+
+        with open('resultado.txt', 'a') as arquivo:
+            arquivo.write(f"\nTempo total para geração do arquivo 'resultado.txt': {tdelta.total_seconds()} segundos")
+        
+        print("Execução do programa concluída com sucesso.")
+    except socket.error as e:
+        print("Erro de conexão: ", str(e))
+        sys.exit(1)
 
 if __name__ == '__main__':
-    n = 32 # Número de processos a serem criados
-    r = 100  # número de vezes que cada processo executará a região crítica
-    k = 1  # tempo de espera em segundos após escrever no arquivo
-
-    processos = []
-    for _ in range(n):
-        processo = Processo(r=r, k=k)
-        processos.append(processo)
-        processo.start()
-
-    for processo in processos:
-        processo.join()
-
-    with open('resultado.txt', 'a') as arquivo:
-        arquivo.write(f'\nQuantidade de processos(n): {n}\n')
-        arquivo.write(f'Quantidade de vezes que cada processo executou a região crítica(r): {r}\n')
-        arquivo.write(f'Tempo definido em segundos para cada execução na região crítica(k): {k}\n')
-
-    # Calculating the time from the file
-    with open('resultado.txt', 'r') as f:
-        lines = f.readlines()
-        first_line_time = lines[1].split('|')[1].strip()  # First timestamp
-        last_line_time = lines[-5].split('|')[1].strip()  # Last timestamp
-
-        FMT = '%H:%M:%S.%f'
-        tdelta = datetime.strptime(last_line_time, FMT) - datetime.strptime(first_line_time, FMT)
-
-    with open('resultado.txt', 'a') as arquivo:
-        arquivo.write(f"\nTempo total para geração do arquivo 'resultado.txt': {tdelta.total_seconds()} segundos")
+    main()
